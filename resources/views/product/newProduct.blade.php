b3002ea8a4f70a407052b07278c698029357625a
hyunh222 <hyunny0905@gmail.com>
2024-03-10 17:46:21 +0900

    rename: 신규 상품 `include`형식으로 변경 및 파일명 수정

--- resources/views/product/newProduct.blade.php
+++ resources/views/product/newProduct.blade.php
@@ -120,68 +120,10 @@
     @include('product.best-new-product-ext')
 
     {{-- 신규 상품 등록 업체 --}}
+    @include('product.inc-new-product-company')
+
+    {{-- 신규 등록 업체 --}}
     @include('product.inc-new-product')
-
-    <section class="sub_section sub_section_bot">
-        <div class="inner">
-            <div class="main_tit mb-8 flex justify-between items-center">
-                <div class="flex items-center gap-4">
-                    <h3>신규 등록 상품</h3>
-                    <button class="zoom_btn flex items-center gap-1" onclick="modalOpen('#zoom_view-modal-new')"><svg><use xlink:href="/img/icon-defs.svg#zoom"></use></svg>확대보기</button>
-                </div>
-            </div>
-            <div class="sub_filter">
-                <div class="filter_box">
-                    <button onclick="modalOpen('#filter_category-modal')">카테고리</button>
-                    <button onclick="modalOpen('#filter_location-modal')">소재지</button>
-                    <button onclick="modalOpen('#filter_align-modal')">최신 상품 등록순</button>
-                </div>
-                <div class="total">전체 428개</div>
-            </div>
-            <div class="sub_filter">
-                <div class="filter_box">
-                    <button class="on" onclick="modalOpen('#filter_category-modal')">카테고리 <b class="txt-primary">3</b></button>
-                    <button class="on" onclick="modalOpen('#filter_location-modal')">소재지 <b class="txt-primary">2</b></button>
-                    <button onclick="modalOpen('#filter_align-modal')">최신 상품 등록순</button>
-                </div>
-                <div class="total">전체 428개</div>
-            </div>
-            <div class="sub_filter_result">
-                <div class="filter_on_box">
-                    <div class="category">
-                        <span>소파/거실 <button onclick="filterRemove(this)"><svg><use xlink:href="/img/icon-defs.svg#x"></use></svg></button></span>
-                        <span>식탁/의자 <button onclick="filterRemove(this)"><svg><use xlink:href="/img/icon-defs.svg#x"></use></svg></button></span>
-                        <span>수납/서랍장/옷장 <button onclick="filterRemove(this)"><svg><use xlink:href="/img/icon-defs.svg#x"></use></svg></button></span>
-                    </div>
-                    <div class="location">
-                        <span>인천 <button onclick="filterRemove(this)"><svg><use xlink:href="/img/icon-defs.svg#x"></use></svg></button></span>
-                        <span>광주 <button onclick="filterRemove(this)"><svg><use xlink:href="/img/icon-defs.svg#x"></use></svg></button></span>
-                    </div>
-                </div>
-                <button class="refresh_btn">초기화 <svg><use xlink:href="/img/icon-defs.svg#refresh"></use></svg></button>
-            </div>
-
-            <div class="relative">
-                <ul class="prod_list">
-                    @foreach ($list as $item )
-                        <li class="prod_item">
-                            <div class="img_box">
-                                <a href="/product/detail/{{ $item->idx }}"><img src="{{ $item->imgUrl }}" alt=""></a>
-                                <button class="zzim_btn prd_{{ $item->idx }} {{ ($item->isInterest == 1) ? 'active' : '' }}" pidx="{{ $item->idx }}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
-                            </div>
-                            <div class="txt_box">
-                                <a href="/product/detail/{{ $item->idx }}">
-                                    <span>{{$item->companyName}}</span>
-                                    <p>{{ $item->name }}</p>
-                                    <b>{{ number_format($item->price, 0) }}원</b>
-                                </a>
-                            </div>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    </section>
     @php $product = $list @endphp
     @include('product.new-product-ext')
 </div>
