109ac499e08134adead453e461201b4310001731
hyunh222 <hyunny0905@gmail.com>
2024-03-20 15:16:26 +0900

    feat: [뉴스정보] 가구 소식 - 기능 수정
    - 본문 내용 일부 내용 미리 보기 출력 시, HTML 엔티티로 표시안되게 수정

--- resources/views/magazine/furnitureNews.blade.php
+++ resources/views/magazine/furnitureNews.blade.php
@@ -43,7 +43,7 @@
                         <div class="txt_box">
                             <a href="/magazine/furniture/detail/{{$item->idx}}">
                                 <div class="tit">{{ $item->title }}</div>
-                                <div class="desc">{!! Illuminate\Support\Str::limit(strip_tags($item->content), $limit = 40, $end = '...') !!}</div>
+                                <div class="desc">{!! Illuminate\Support\Str::limit(html_entity_decode(strip_tags($item->content)), $limit = 40, $end = '...') !!}</div>
                                 <span>{{ Carbon\Carbon::parse($item->register_time)->format('Y.m.d') }}</span>
                             </a>
                         </div>
