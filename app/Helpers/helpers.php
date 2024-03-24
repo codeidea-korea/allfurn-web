dc65f26e71adb336ecd74467c562bd3e25ce316e
kevin <kr.kevin.kang@gmail.com>
2024-03-25 07:40:14 +0900

    도메업체 > 이달의 도매 개발(진행중)

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -56,3 +56,53 @@ function getDeviceType() {
     }
     return '';
 }
+
+// 도매업체 키(company_idx) 기준 배열 정리
+function setArrayNumer( $arr )
+{
+    if( empty( $arr ) ) return false;
+
+    $cnt = array();
+    $nData = array();
+    $tmp = 0;
+    foreach( $arr AS $data ) {
+        $cIdx = $data->company_idx;
+
+        $cnt[$cIdx] = ( !isset( $cnt[$cIdx] ) ) ? 0 : $cnt[$cIdx] + 1;
+
+        if( empty( $nData['info'][$cIdx] ) ) {
+            $nData['info'][$cIdx]['company_idx'] = $data->company_idx;
+            $nData['info'][$cIdx]['companyName'] = $data->companyName;
+            $nData['info'][$cIdx]['companyRegion'] = $data->companyRegion;
+        }
+
+        $nData['info'][$cIdx]['category_name'] = array();
+        if( $data->category_name != '' && !in_array( $data->category_name, $nData['info'][$cIdx]['category_name'] ) ) {
+
+            $nData['info'][$cIdx]['category_name'][$cnt[$cIdx]] = $data->category_name;
+        }
+
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['idx']  = $data->idx;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['product_idx']  = $data->product_idx;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['admin_idx']  = $data->admin_idx;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['start_date']  = $data->start_date;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['end_date']  = $data->end_date;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['ad_location']  = $data->ad_location;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['state']  = $data->state;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['price']  = $data->price;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['ad_code']  = $data->ad_code;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['order_idx']  = $data->order_idx;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['app_link']  = $data->app_link;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['app_link_type']  = $data->app_link_type;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['web_link']  = $data->web_link;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['web_link_type']  = $data->web_link_type;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['content']  = $data->content;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['is_open']  = $data->is_open;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['is_delete']  = $data->is_delete;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['register_time']  = $data->register_time;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['name']  = $data->name;
+        $nData['info'][$cIdx]['items'][$cnt[$cIdx]]['imgUrl'] = $data->imgUrl;
+    }
+
+    return $nData;
+}
