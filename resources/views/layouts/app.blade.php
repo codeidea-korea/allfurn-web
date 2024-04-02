1af49ac20223625d000bf73e046cf8d09f7a5b0b
hyunh222 <hyunny0905@gmail.com>
2024-04-02 15:00:52 +0900

    feat: cdn 문제로 css 파일 추가

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,13 +28,14 @@
     <link rel="stylesheet" href="/css/common.css?{{ date('Ymdhis') }}">
     <link rel="stylesheet" href="/css/style.css?{{ date('Ymdhis') }}">
     <link rel="stylesheet" href="/css/rubin.css?{{ date('Ymdhis') }}">
+    <link rel="stylesheet" href="/css/output.css?{{ date('Ymdhis') }}">
     <!-- flatpickr(datepicker) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
 
     <script src="/js/jquery-1.12.4.js?{{ date('Ymdhis') }}"></script>
     <script src="/js/jquery-ui-1.13.1.js?{{ date('Ymdhis') }}"></script>
     <script src="/js/pci.js?{{ date('Ymdhis') }}"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="/js/common.js?{{ date('Ymdhis') }}"></script>
     <!-- flatpickr(datepicker) -->
