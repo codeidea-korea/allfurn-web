6c3ae10858b0fd10971e386ac1d4a0b1ffc0b553
p2squareb <p2squareb@gmail.com>
2024-04-08 18:12:40 +0900

    메인 할인,이벤상품 수정1

--- resources/views/m/product/plan_discount_list.blade.php
+++ resources/views/m/product/plan_discount_list.blade.php
@@ -1,1 +1,39 @@
-123
+@extends('layouts.app')
+
+@section('content')
+@include('layouts.header')
+<div id="content">
+    <section class="sub">
+        <div class="inner">
+            <div class="main_tit mb-8 flex justify-between items-center">
+                <div class="flex items-center gap-4">
+                    <h3>Best 기획전</h3>
+                </div>
+            </div>
+            <div class="relative">
+                @if( $list->count() > 0 )
+                <ul class="prod_list grid2">
+                    @foreach( $list AS $l => $item )
+                    <li class="prod_item type02">
+                        <div class="img_box">
+                            <a href="/product/detail/{{$item->idx}}">
+                                <img src="{{$item->imgUrl}}" alt="">
+                                <span><b>{{$item->subtext1}}</b><br/>{{$item->subtext2}}</span>
+                            </a>
+                            <button class="zzim_btn prd_{{$item->idx}} {{($item->isInterest==1)?'active':''}}" pIdx="{{$item->idx}}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
+                        </div>
+                        <div class="txt_box">
+                            <a href="/product/detail/{{$item->idx}}">
+                                <strong>{{$item->content}}</strong>
+                                <span>{{$item->company_name}}</span>
+                            </a>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
+                @endif
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
