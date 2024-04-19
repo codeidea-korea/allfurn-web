6a9deeb1341f4c712c38741b088f55d9a083bba4
hyunh222 <hyunny0905@gmail.com>
2024-04-19 19:24:34 +0900

    feat: 업체 상세 모바일 기능 추가

--- resources/views/m/wholesaler/detail.blade.php
+++ resources/views/m/wholesaler/detail.blade.php
@@ -15,8 +15,8 @@
             </div>
         </div>
         <div class="link_box">
-            <button class="addLike {{ ($data['info']->isLike == 1) ? 'active' : '' }}" onClick="addLike({{$data['info']->idx}});"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
-            <button onclick="copyUrl()"><svg><use xlink:href="/img/icon-defs.svg#share"></use></svg></button>
+            <button class="addLike {{ ($data['info']->isLike == 1) ? 'active' : '' }}" onClick="addLike({{$data['info']->idx}});" onclick="companyLike(this)"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
+            <button onClick="copyUrl();"><svg><use xlink:href="/img/icon-defs.svg#share"></use></svg></button>
         </div>
         <div class="inner">
             <div class="info">
@@ -45,8 +45,8 @@
                         </p>
                     </div>
                     <div class="btn_box">
-                        <button class="btn btn-primary-line phone" onClick="location.href='/message';"><svg class="w-5 h-5"><use xlink:href="/img/icon-defs.svg#inquiry"></use></svg>문의하기</button>
-                        <button class="btn btn-primary" onclick="modalOpen('#company_phone-modal')"><svg class="w-5 h-5"><use xlink:href="/img/m/icon-defs.svg#phone_white"></use></svg>전화번호 확인하기</button>
+                        <button class="btn btn-primary-line phone" onClick="sendMessage()"><svg class="w-5 h-5"><use xlink:href="/img/icon-defs.svg#inquiry"></use></svg>문의하기</button>
+                        <button class="btn btn-primary" onClick="location.href='tel:{{$data['info']->phone_number}}';"><svg class="w-5 h-5"><use xlink:href="/img/m/icon-defs.svg#phone_white"></use></svg>전화하기</button>
                     </div>
                 </div>
             </div>
@@ -61,7 +61,7 @@
                     </dt>
                     <dd>
                         공휴일/ 토요일/ 일요일 휴무입니다.<br/>
-                        통화 부재 시 문자 남겨 주시면 전화드리겠습니다.
+                        통화 부재 시 문자 남겨 주시면 전화드리겠습니다. 
                     </dd>
                 </dl>
             </div>
@@ -78,117 +78,95 @@
         <div class="tab_content">
             <!-- 판매상품 -->
             <div class="active">
-                @if( count( $data['event'] ) > 0 )
-                <div class="box">
-                    <div class="inner">
-                        <div class="main_tit mb-5">
-                            <h3>{{$data['info']->company_name}} 이벤트 상품</h3>
-                        </div>
-                        <div class="relative">
-                            @foreach( $data['event'] AS $e => $event )
-                            @if( $e == 0 )
-                            <ul class="prod_list grid1 mb-10">
-                            @elseif( $e == 1 )
-                            </ul>
-                            <ul class="prod_list">
-                            @endif
-                                <li class="prod_item type02">
-                                    <div class="img_box">
-                                        <a href="/product/detail/{{$event->idx}}">
-                                            <img src="{{$event->imgUrl}}" alt="{{$item->name}}">
-                                            <!-- span><b>호텔같은 내 침실로!</b><br>#20조 한정 할인 특가 #호텔형 침대</span //-->
-                                        </a>
-                                        <button class="zzim_btn prd_{{$event->idx}} {{ ($event->isInterest == 1) ? 'active' : '' }}" pidx="{{$event->idx}}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
-                                    </div>
-                                    <div class="txt_box">
-                                        <a href="./prod_detail.php">
-                                            <strong>{{$item->name}}</strong>
-                                            <span>{{$data['info']->company_name}}</span>
-                                        </a>
-                                    </div>
-                                </li>
-                            @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                @endif
-                @if( count( $data['recommend'] ) > 0 )
-                <div class="box overflow-hidden">
-                    <div class="inner">
-                        <div class="main_tit mb-10">
-                            <h3>{{$data['info']->company_name}} 추천 상품</h3>
-                        </div>
-                        <div class="relative recommand_prod">
-                            <div class="slide_box prod_slide">
-                                <ul class="swiper-wrapper">
-                                    @foreach( $data['recommend'] AS $r => $recommend )
-                                    <li class="swiper-slide prod_item">
+                @if(count($data['event']) > 0)
+                    <div class="box">
+                        <div class="inner">
+                            <div class="main_tit mb-5">
+                                <h3>올펀가구 이벤트 상품</h3>
+                            </div>
+                            <div class="relative">
+                                {{-- <ul class="prod_list grid1 mb-10">
+                                    <li class="prod_item type02">
                                         <div class="img_box">
-                                            <a href="/product/detail/{{$recommend->idx}}"><img src="{{$recommend->imgUrl}}" alt="{{$recommend->name}}"></a>
-                                            <button class="zzim_btn prd_{{$recommend->idx}} {{ ($recommend->isInterest == 1) ? 'active' : '' }}" pidx="{{$recommend->idx}}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
+                                            <a href="./prod_detail.php">
+                                                <img src="/img/sale_thumb.png" alt="">
+                                                <span><b>호텔같은 내 침실로!</b><br>#20조 한정 할인 특가 #호텔형 침대</span>
+                                            </a>
+                                            <button class="zzim_btn"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
                                         </div>
                                         <div class="txt_box">
-                                            <a href="/product/detail/{{$recommend->idx}}">
-                                                <span>{{$data['info']->company_name}}</span>
-                                                <p>{{$recommend->name}}</p>
-                                                <b>
-                                                    @if( $recommend->pay_type == 1 )
-                                                        {{number_format( $recommend->price )}}원
-                                                    @else
-                                                        {{$recommend->pay_type_text}}
-                                                    @endif
-                                                </b>
+                                            <a href="./prod_detail.php">
+                                                <strong>내 침실을 휴향지 호텔 처럼!  20조 한정 할인 특가를 진행합니다.</strong>
+                                                <span>올펀가구</span>
                                             </a>
                                         </div>
                                     </li>
+                                </ul> --}}
+                                <ul class="prod_list">
+                                    @foreach( $data['event'] AS $event )
+                                        <li class="prod_item">
+                                            <div class="img_box">
+                                                <a href="/product/detail/{{$event->idx}}"><img src="{{$event->imgUrl}}" alt=""></a>
+                                                <button class="zzim_btn prd_{{ $event->idx }} {{ ($event->isInterest == 1) ? 'active' : '' }}" pidx="{{ $event->idx }}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
+                                            </div>
+                                            <div class="txt_box">
+                                                <a href="/product/detail/{{$event->idx}}">
+                                                    <span>{{$event->company_name}}</span>
+                                                    <p>{{$event->name}}</p>
+                                                    <b>{{$event->is_price_open ? number_format($event->price, 0).'원': $event->price_text}}</b>
+                                                </a>
+                                            </div>
+                                        </li>
                                     @endforeach
                                 </ul>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endif
-
-                @if( count( $data['list'] ) > 0 )
+                @if($data['recommend']->count() > 0)
+                    <div class="box overflow-hidden">
+                        <div class="inner">
+                            <div class="main_tit mb-10">
+                                <h3>{{$data['info']->company_name}} 추천 상품</h3>
+                            </div>
+                            <div class="relative recommand_prod">
+                                <div class="slide_box prod_slide">
+                                    <ul class="swiper-wrapper">
+                                        @foreach($data['recommend'] as $item)
+                                            <li class="swiper-slide prod_item">
+                                                <div class="img_box">
+                                                    <a href="/product/detail/{{$item->idx}}"><img src="{{$item->imgUrl}}" alt=""></a>
+                                                    <button class="zzim_btn prd_{{ $item->idx }} {{ ($item->isInterest == 1) ? 'active' : '' }}" pidx="{{ $item->idx }}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
+                                                </div>
+                                                <div class="txt_box">
+                                                    <a href="/product/detail/{{$item->idx}}">
+                                                        <span>{{$item->company_name}}</span>
+                                                        <p>{{$item->name}}</p>
+                                                        <b>{{$item->is_price_open ? number_format($item->price, 0).'원': $item->price_text}}</b>
+                                                    </a>
+                                                </div>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
                 <div class="box">
                     <div class="inner">
                         <div class="sub_filter">
                             <div class="filter_box">
-                                <button onclick="modalOpen('#filter_category-modal')">카테고리</button>
-                                <button onclick="modalOpen('#filter_align-modal')">최신 상품 등록순</button>
+                                <button onclick="modalOpen('#filter_category-modal')">카테고리 <b class='txt-primary'></b></button>
+                                <button onclick="modalOpen('#filter_align-modal03')">최신순</button>
                             </div>
-                            <div class="total">전체 {{$data['list']->count()}}개</div>
+                            <div class="total">전체 0개</div>
                         </div>
                         <div class="relative">
-                            <ul class="prod_list">
-                                @foreach( $data['list'] AS $l => $list )
-                                <li class="prod_item">
-                                    <div class="img_box">
-                                        <a href="/product/detail/{{$list->idx}}"><img src="{{$list->imgUrl}}" alt="{{$list->name}}"></a>
-                                        <button class="zzim_btn prd_{{$list->idx}} {{ ($list->isInterest == 1) ? 'active' : '' }}" pidx="{{$list->idx}}"><svg><use xlink:href="/img/icon-defs.svg#zzim"></use></svg></button>
-                                    </div>
-                                    <div class="txt_box">
-                                        <a href="/product/detail/{{$list->idx}}">
-                                            <span>{{$data['info']->company_name}}</span>
-                                            <p>{{$list->name}}</p>
-                                            <b>
-                                                @if( $list->pay_type == 1 )
-                                                    {{number_format( $list->price )}}원
-                                                @else
-                                                    {{$list->pay_type_text}}
-                                                @endif
-                                            </b>
-                                        </a>
-                                    </div>
-                                </li>
-                                @endforeach
-                            </ul>
+                            <ul class="prod_list"></ul>
                         </div>
-
                     </div>
                 </div>
-                @endif
             </div>
 
             <!-- 업체정보 -->
@@ -241,7 +219,7 @@
         </div>
     </div>
     <!-- 업체 전화번호 모달 -->
-    <div class="modal" id="company_phone-modal">
+    {{-- <div class="modal" id="company_phone-modal">
         <div class="modal_bg" onclick="modalClose('#company_phone-modal')"></div>
         <div class="modal_inner modal-md">
             <button class="close_btn" onclick="modalClose('#company_phone-modal')"><svg class="w-11 h-11"><use xlink:href="/img/icon-defs.svg#Close"></use></svg></button>
@@ -260,7 +238,7 @@
                 <button class="btn btn-primary w-full" onclick="modalClose('#company_phone-modal')">확인</button>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!-- 공유 팝업 -->
     <div class="modal" id="alert-modal">
         <div class="modal_bg" onclick="modalClose('#alert-modal')"></div>
@@ -299,11 +277,7 @@
 
     });
 
-    /*const companyLike = (item)=>{
-        $(item).toggleClass('active')
-    }*/
-
-    // 좋아요
+    // 업체 좋아요
     function addLike(idx) {
         if (isProc) {
             return;
@@ -345,6 +319,136 @@
         document.body.removeChild(dummy);
 
         modalOpen('#alert-modal')
+    }
+
+    //문의하기
+    function sendMessage() {
+        idx='';
+        type=''
+        if ($(location).attr('href').includes('/product/detail')) {
+            idx = $(location).attr('pathname').split('/')[3];
+            type = 'product';
+        } else if ($(location).attr('href').includes('/wholesaler/detail/')) {
+            idx = $(location).attr('pathname').split('/')[3];
+            type = 'wholesaler';
+        }
+
+        $.ajax({
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            url                : '/message/send',
+            data            : {
+                'idx'       : idx,
+                'type'      : type,
+                'message'   : '상품 문의드립니다.'
+            },
+            type            : 'POST',
+            dataType        : 'json',
+            success        : function(result) {
+                if (result.result == 'success') {
+                    location.href = "/message/room?room_idx=" + result.roomIdx;
+                } else {
+
+                }
+            }
+        });
+    }
+
+    /* --- 전체 상품 비동기 조회 --- */
+    $(document).ready(function(){
+        setTimeout(() => {
+            loadProductList();
+        }, 50);
+    })
+
+    window.addEventListener('scroll', function() {
+        if ((window.pageYOffset || document.documentElement.scrollTop) + window.innerHeight + 20 >= document.documentElement.scrollHeight && !isLoading && !isLastPage) {
+            loadProductList();
+        }
+    });
+
+    $(document).on('click', '[id^="filter"] .btn-primary', function() { 
+        loadProductList(true, $(this));
+    });
+
+    // 카테고리 삭제
+    const filterRemove = (item)=>{
+        $(item).parents('span').remove(); //해당 카테고리 삭제
+        $("#" + $(item).data('id')).prop('checked', false);//모달 안 체크박스에서 check 해제
+
+        loadProductList(true);
+    }
+
+    let isLoading = false;
+    let isLastPage = false;
+    let currentPage = 0;
+    let firstLoad = true;
+    function loadProductList(needEmpty, target) {
+        isLoading = true;
+        if(needEmpty) currentPage = 0;
+
+        $.ajax({
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            url: '/wholesaler/wholesalerAddProduct',
+            method: 'GET',
+            data: { 
+                'page': ++currentPage,
+                'categories' : getIndexesOfSelectedCategory().join(','),
+                'orderedElement' : $("#filter_align-modal03 .radio-form:checked").val(),
+                'company_idx'   : '{{$data['info']->idx}}'
+            }, 
+            beforeSend : function() {
+                if(target) {
+                    target.prop("disabled", true);
+                }
+            },
+            success: function(result) {
+                console.log(result);
+                if(needEmpty) {
+                    $(".box").last().find(".prod_list").empty();
+                }
+                
+                $(".box").last().find(".prod_list").append(result.data.html);
+
+                $(".total").text('전체 ' + result.total.toLocaleString('ko-KR') + '개');
+
+                if(target) {
+                    target.prop("disabled", false);
+                    modalClose('#' + target.parents('[id^="filter"]').attr('id'));
+                }
+
+                isLastPage = currentPage === result.last_page;
+            }, 
+            complete : function () {
+                displaySelectedCategories();
+                displaySelectedOrders();
+                isLoading = false;
+            }
+        })
+    }
+
+    function getIndexesOfSelectedCategory() {
+        let categories = [];
+        $("#filter_category-modal .check-form:checked").each(function(){
+            categories.push($(this).attr('id'));
+        });
+
+        return categories;
+    }
+
+    function displaySelectedCategories() {
+        let totalOfSelectedCategories = $("#filter_category-modal .check-form:checked").length;
+        if(totalOfSelectedCategories === 0) {
+            $(".sub_filter .filter_box button").eq(0).find('.txt-primary').text("");
+            $(".sub_filter .filter_box button").eq(0).removeClass('on');
+        } else {
+            $(".sub_filter .filter_box button").eq(0).find('.txt-primary').text(totalOfSelectedCategories);
+            $(".sub_filter .filter_box button").eq(0).addClass('on');
+        }
+    } 
+
+    function displaySelectedOrders() {
+        $(".sub_filter .filter_box button").eq(1)
+            .text($("#filter_align-modal03 .radio-form:checked").siblings('label').text());
     }
 </script>
 @endsection
