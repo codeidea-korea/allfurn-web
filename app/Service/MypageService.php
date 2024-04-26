da177ba89d54400a90ce95d784a377e9682ef987
p2squareb <p2squareb@gmail.com>
2024-04-26 22:15:40 +0900

    일반회원 견적서보내기 접근 금지

--- app/Service/MypageService.php
+++ app/Service/MypageService.php
@@ -2254,6 +2254,7 @@ class MypageService
                     ORDER BY p.idx";
                 break;
             default:
+                $sql = "select * from AF_product where idx=0";
                 break;
         }
         $estimateMulti = DB::select($sql);
