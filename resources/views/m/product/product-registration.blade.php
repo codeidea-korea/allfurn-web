09de76894c1b0bc017b886a3b19cb04386aa2d30
p2squareb <p2squareb@gmail.com>
2024-05-02 17:44:42 +0900

    상품등록관리 텍스트에디터 cdn 오류 수정

--- resources/views/m/product/product-registration.blade.php
+++ resources/views/m/product/product-registration.blade.php
@@ -374,8 +374,8 @@
 
 
 
-<link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
+<link href="/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/js/froala_editor.pkgd.min.js"></script>
 <script type="text/javascript">
 const productIdx = "{{ $productIdx }}";
 var storedFiles = [];
