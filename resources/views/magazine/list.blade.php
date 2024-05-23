73a1d5f40d8a39774886c4e1b52dd8ee9703a07c
p2squareb <p2squareb@gmail.com>
2024-05-23 16:42:06 +0900

    일일가구 뉴스 24시간 new 뱃지 추가

--- resources/views/magazine/list.blade.php
+++ resources/views/magazine/list.blade.php
@@ -49,7 +49,9 @@
                     <ul class="news_list">
                         @foreach($articles as $item)
                             <li><a href="{{ $item->content_type == 1 ? '/magazine/daily/detail/'.$item->idx : $item->blank_link  }}">
-                                <!-- <span class="new">NEW</span> -->
+                                @if (now()->diffInHours($item->register_time) < 24)
+                                    <span class="new">NEW</span>
+                                @endif
                                 <div class="tit">{{ $item->title }}</div>
                                 <div class="desc">{!! Illuminate\Support\Str::limit(html_entity_decode(strip_tags($item->content)), $limit = 140, $end = '...') !!}</div>
                                 <span>{{ Carbon\Carbon::parse($item->register_time)->format('Y.m.d') }}</span>
