93a95c074c67b67d9571deb7771ec62f05de1211
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2024-05-29 16:41:31 +0900

    모바일 뒤로가기버튼 수정

--- resources/views/layouts/header_m.blade.php
+++ resources/views/layouts/header_m.blade.php
@@ -75,7 +75,7 @@
 
 @if(Route::currentRouteName() != '')
     <div id="move_back_btn" class="{{($header_depth=='mypage' || $header_depth=='community' || $header_depth=='talk'|| $header_depth=='thismonth' )?'hidden':'' }}">
-        <a href="javascript:history.back()">뒤로<br/>가기</a>
+        <a href="javascript:history.back()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg></a>
     </div>
 @endif
 
