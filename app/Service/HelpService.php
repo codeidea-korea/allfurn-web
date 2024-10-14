39442fb78bef673fbabe108036cb81051635f111
minwoojeon <botbinoo@naver.com>
2024-10-14 18:31:36 +0900

    Update HelpService.php

--- app/Service/HelpService.php
+++ app/Service/HelpService.php
@@ -87,8 +87,19 @@ class HelpService
 
         $query = DB::table('AF_guide')->where('is_open', 1)->where('is_delete', 0);
 
+        if(!empty($params['is_pick'])) {
+            $query->where('is_pick', 1);
+        }
+        if(!empty($params['npick'])) {
+            $query->where('is_pick', 0);
+        }
         $data['count'] = $query->count();
-        $list = $query->orderBy('idx', 'desc')->offset($offset)->limit($limit)->get();
+        
+        if(!empty($params['is_pick'])) {
+            $list = $query->orderBy('update_time', 'asc')->offset($offset)->limit($limit)->get();
+        } else {
+            $list = $query->orderBy('idx', 'desc')->offset($offset)->limit($limit)->get();
+        }
         $data['list'] = $list;
         $data['pagination'] = paginate($params['offset'], $params['limit'], $data['count']);
         return $data;
