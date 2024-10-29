ed778488a88ab5169337914f32b629c848391277
minwoojeon <botbinoo@naver.com>
2024-10-29 17:15:49 +0900

    Update inc-product-common2.blade.php

--- resources/views/product/inc-product-common2.blade.php
+++ resources/views/product/inc-product-common2.blade.php
@@ -1,10 +1,10 @@
 @foreach ($list as $item )
     <li class="prod_item">
         <div class="img_box custom_input2">
-            <input type="checkbox" id="new_esti_{{ $item->idx }}" name="product_idxs[]" value="{{ $item->idx }}" />
             <label for="new_esti_{{ $item->idx }}">
                 <img src="{{ $item->imgUrl }}" alt="">
             </label>
+            <input type="checkbox" style="right: 0px;bottom: 0px;" id="new_esti_{{ $item->idx }}" name="product_idxs[]" value="{{ $item->idx }}" />
         </div>
         <div class="txt_box">
             <a href="/product/detail/{{ $item->idx }}">
