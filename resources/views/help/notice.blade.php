78f522c18adf6425dd404751aa1a6e78017a4a4d
minwoojeon <botbinoo@naver.com>
2025-03-06 17:06:37 +0900

    Update notice.blade.php

--- resources/views/help/notice.blade.php
+++ resources/views/help/notice.blade.php
@@ -16,7 +16,7 @@
                             <span class="text-sm text-stone-400">{{ date('Y.m.d', strtotime($row->register_time)) }}</span>
                         </div>
                     </button>
-                    <div class="accordion-body hidden p-5 bg-stone-50">
+                    <div class="accordion-body p-5 bg-stone-50" @if($noticeIdx == $row->idx) style="display: block;" @else hidden @endif>
                             {!! $row->content !!} 
                     </div>
                 </div>
@@ -32,7 +32,7 @@
                             <span class="text-sm text-stone-400">{{ date('Y.m.d', strtotime($row->register_time)) }}</span>
                         </div>
                     </button>
-                    <div class="accordion-body hidden p-5 bg-stone-50" @if($noticeIdx == $row->idx) style="display: block;"  @endif>
+                    <div class="accordion-body p-5 bg-stone-50" @if($noticeIdx == $row->idx) style="display: block;" @else hidden  @endif>
                             {!! $row->content !!} 
                     </div>
                 </div>
