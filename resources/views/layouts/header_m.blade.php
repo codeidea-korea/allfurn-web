7513662e545da6cf7fbb0d8a881be9787fe715d4
minwoojeon <botbinoo@naver.com>
2025-04-18 15:04:35 +0900

    Update header_m.blade.php

--- resources/views/layouts/header_m.blade.php
+++ resources/views/layouts/header_m.blade.php
@@ -1,4 +1,12 @@
-
+<script>
+    var hasNotDefinedUserType = {{ Auth::user()['is_undefined_type'] }} === 1;
+    function gotoLink(url) {
+        if(hasNotDefinedUserType) {
+            location.href = location.href.replace(location.pathname, '') + '/mypage/company-account';
+        }
+        location.href = location.href.replace(location.pathname, '') + url;
+    }
+</script>
 <header class="{{ $only_quick=='yes'?'hidden':'' }}">
     <div class="header_top {{ ($header_depth=='mypage' || $header_depth=='prodlist') ? 'hidden' : '' }}">
         <div class="inner">
@@ -67,9 +75,9 @@
         <li class="{{$header_depth=='like'?'active':'' }}"><a href="/like/product"><svg><use xlink:href="/img/m/icon-defs.svg#quick_like"></use></svg><span>좋아요</span></a></li>
         <li class="{{($header_depth!=='like'&&$header_depth!=='talk'&&$header_depth!=='mypage'&&$header_depth!=='category') ?'active':'' }}"><a href="/"><svg><use xlink:href="/img/m/icon-defs.svg#quick_home"></use></svg><span>홈</span></a></li>
         <!-- 안읽은 메세지 수 숫자로 표기 -->
-        <li class="{{$header_depth=='talk'?'active':'' }}"><a href="/message"><svg><use xlink:href="/img/m/icon-defs.svg#quick_talk"></use></svg><span>올톡</span>@if (unCheckedAllTalkCount() > 0) <b class="_totChatMessageCount">{{ unCheckedAllTalkCount() }}</b> @endif</a></li>
+        <li class="{{$header_depth=='talk'?'active':'' }}"><a href="javascript:gotoLink('/message');"><svg><use xlink:href="/img/m/icon-defs.svg#quick_talk"></use></svg><span>올톡</span>@if (unCheckedAllTalkCount() > 0) <b class="_totChatMessageCount">{{ unCheckedAllTalkCount() }}</b> @endif</a></li>
         <!-- 확인하지 않은 내용 있을 때 붉은점 noread 클래스 추가 -->
-        <li class="{{$header_depth=='mypage'?'active':'' }} {{ unCheckedMyAllFurn() ? 'noread' : '' }}"><a href="/mypage"><svg><use xlink:href="/img/m/icon-defs.svg#quick_my"></use></svg><span>마이올펀</span></a></li>
+        <li class="{{$header_depth=='mypage'?'active':'' }} {{ unCheckedMyAllFurn() ? 'noread' : '' }}"><a href="javascript:gotoLink('/mypage');"><svg><use xlink:href="/img/m/icon-defs.svg#quick_my"></use></svg><span>마이올펀</span></a></li>
     </ul>
 </div>
 
