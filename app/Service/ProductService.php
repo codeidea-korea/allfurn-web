7c81eb0d033f7c187705d054ccb965d6de68779e
minwoojeon <botbinoo@naver.com>
2025-10-23 20:30:22 +0900

    fix: 검색 결과 오류 수정

--- app/Service/ProductService.php
+++ app/Service/ProductService.php
@@ -1259,31 +1259,9 @@ class ProductService
             DB::raw('(CASE WHEN aprod.company_type = "W" THEN aw.company_name
                     WHEN aprod.company_type = "R" THEN ar.company_name
                     ELSE "" END) as companyName,
-                    '. $this->generateMappingThumbnailQuery('at') . ',
                 (SELECT if(count(pi.idx) > 0, 1, 0) FROM AF_product_interest as pi WHERE pi.product_idx = aprod.idx AND pi.user_idx = '.Auth::user()->idx.') as isInterest,
                 (SELECT COUNT(*) cnt FROM AF_order as ao WHERE ao.product_idx = aprod.idx) as orderCnt
             '))
-            ->leftJoin('AF_attachment as at', function($query) {
-                $query->on('at.idx', DB::raw('SUBSTRING_INDEX(aprod.attachment_idx, ",", 1)'));
-            })
-            ->leftjoin('AF_mapping_thumb_attachment as mpg_at', function($query) {
-                $query->on('mpg_at.main_attach_idx', 'at.idx');
-            })
-            ->leftjoin('AF_attachment as at100', function($query) {
-                $query->on('at100.idx', 'mpg_at.size_100_attach_idx');
-            })
-            ->leftjoin('AF_attachment as at200', function($query) {
-                $query->on('at200.idx', 'mpg_at.size_200_attach_idx');
-            })
-            ->leftjoin('AF_attachment as at400', function($query) {
-                $query->on('at400.idx', 'mpg_at.size_400_attach_idx');
-            })
-            ->leftjoin('AF_attachment as at600', function($query) {
-                $query->on('at600.idx', 'mpg_at.size_600_attach_idx');
-            })
-            ->leftjoin('AF_attachment as at1000', function($query) {
-                $query->on('at1000.idx', 'mpg_at.size_1000_attach_idx');
-            })
             ->leftJoin('AF_wholesale as aw', function($query){
                 $query->on('aw.idx', 'aprod.company_idx')-> where('aprod.company_type', 'W');
             })
@@ -1338,6 +1316,42 @@ class ProductService
         }
         
         $result = $list->paginate(20);
+
+        
+        $prevImagePath = preImgUrl();
+        foreach($result as $product){
+            $representImageIndxes = explode(',', $product->attachment_idx);
+            if(count($representImageIndxes) < 1) {
+                $product->imgUrl = $prevImagePath . '';
+                continue;
+            }
+
+            $mappedImages = DB::table('AF_attachment as at')
+                ->leftjoin('AF_mapping_thumb_attachment as mpg_at', function($query) {
+                    $query->on('mpg_at.main_attach_idx', 'at.idx');
+                })
+                ->leftjoin('AF_attachment as at100', function($query) {
+                    $query->on('at100.idx', 'mpg_at.size_100_attach_idx');
+                })
+                ->leftjoin('AF_attachment as at200', function($query) {
+                    $query->on('at200.idx', 'mpg_at.size_200_attach_idx');
+                })
+                ->leftjoin('AF_attachment as at400', function($query) {
+                    $query->on('at400.idx', 'mpg_at.size_400_attach_idx');
+                })
+                ->leftjoin('AF_attachment as at600', function($query) {
+                    $query->on('at600.idx', 'mpg_at.size_600_attach_idx');
+                })
+                ->leftjoin('AF_attachment as at1000', function($query) {
+                    $query->on('at1000.idx', 'mpg_at.size_1000_attach_idx');
+                })
+                ->selectRaw($this->generateMappingThumbnailQuery('at'))
+                ->where('at.idx', $representImageIndxes[0])
+                ->first();
+
+            $product->imgUrl = $mappedImages->imgUrl;
+        }
+
         return $result;
     }
 
