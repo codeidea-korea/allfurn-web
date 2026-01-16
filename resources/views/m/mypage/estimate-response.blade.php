f638f463e12cfff60df8f55732f65b2470e073a9
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2026-01-16 15:48:59 +0900

    모바일 버전 적용
    모바일 버전 적용

--- resources/views/m/mypage/estimate-response.blade.php
+++ resources/views/m/mypage/estimate-response.blade.php
@@ -485,6 +485,8 @@
                 response_estimate_product_option_price: response_estimate_product_option_price,
                 response_estimate_product_delivery_price: $('#delivery_price').val() || 0,
                 //response_estimate_product_total_price: Number($('input[name=product_each_price]')[index].value) * estimate_data.lists[index].product_count,
+                //response_estimate_product_total_price: estimate_data.lists[index].product_total_price,
+                response_estimate_product_total_price: Number($('.fold_area .prod_info').eq(index).find('.calc_base_price').val()) || 0,
                 response_estimate_product_total_price: estimate_data.lists[index].product_total_price,
                 product_memo:estimate_data.lists[index].product_memo || '',
                 response_estimate_product_memo: estimate_data.lists[index].product_memo || '',
