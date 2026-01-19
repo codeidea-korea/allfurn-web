6caf7a54f5fa7ff5672f15d35353bf45ec7fa09f
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2026-01-19 16:53:44 +0900

    업체 협의 -> 업체 문의 -> 공백으로 롤백
    업체 협의 -> 업체 문의 -> 공백으로 롤백

--- resources/views/m/mypage/inc-estimate-response-check.blade.php
+++ resources/views/m/mypage/inc-estimate-response-check.blade.php
@@ -217,7 +217,7 @@
                                                 <div class="name">단가</div>
                                                 <div>
                                                     @if( $row->is_price_open == 0 ? 1 : 0 )
-                                                        {{ ($row->price_text === null || $row->price_text === '' || $row->price_text === '가격 안내 문구 선택') ? '업체 문의' : $row->price_text }}
+                                                        {{ ($row->price_text === null || $row->price_text === '' || $row->price_text === '가격 안내 문구 선택') ? '' : $row->price_text }}
                                                     @else
                                                         {{ $row->product_total_price }}
                                                     @endif
