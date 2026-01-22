6ab6951aa1c320275401295f6c4522040fe56de9
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2026-01-22 14:01:04 +0900

    모바일 견적서 전송이 금액 차이나는 문제 수정
    모바일 estimate-response계산식 수정

--- resources/views/m/mypage/estimate-response.blade.php
+++ resources/views/m/mypage/estimate-response.blade.php
@@ -487,7 +487,7 @@
                 //response_estimate_product_total_price: Number($('input[name=product_each_price]')[index].value) * estimate_data.lists[index].product_count,
                 //response_estimate_product_total_price: estimate_data.lists[index].product_total_price,
                 response_estimate_product_total_price: Number($('.fold_area .prod_info').eq(index).find('.calc_base_price').val()) || 0,
-                response_estimate_product_total_price: estimate_data.lists[index].product_total_price,
+                //response_estimate_product_total_price: estimate_data.lists[index].product_total_price,
                 product_memo:estimate_data.lists[index].product_memo || '',
                 response_estimate_product_memo: estimate_data.lists[index].product_memo || '',
             });
