4b7966e63de11bfc14950bb104105f653ad65a94
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2026-03-19 11:50:39 +0900

    Update inc-estimate-order.blade.php

--- resources/views/m/mypage/inc-estimate-order.blade.php
+++ resources/views/m/mypage/inc-estimate-order.blade.php
@@ -44,10 +44,9 @@
                                 $productBasePrice = isset($row->price) ? (int) str_replace(',', '', $row->price) : 0;
 
                                 // 가격 비공개 여부 체크 (기존 로직 유지)
-                                if( $row->is_price_open == 0 ) {
+                                if ($row->is_price_open == 0 || $row->price_text == '수량마다 상이' || $row->price_text == '업체 문의') {
                                     $is_any_price_hide = true;
                                 }
-
                                 // -----------------------------------------------------------
                                 // 3. 옵션 가격 계산 ($_each_price) 및 옵션 유무 확인 ($hasOption)
                                 // -----------------------------------------------------------
