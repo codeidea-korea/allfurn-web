dc4acea286f80349e426e49e449d0707b7687d35
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2026-07-06 16:57:04 +0900

    Revert "롤벡"
    This reverts commit c17150c87fbe17784f98a5fea0b05fd1bb64a38d.

--- resources/views/home/mWelcome.blade.php
+++ resources/views/home/mWelcome.blade.php
@@ -243,19 +243,19 @@ function checkMobile(){
                 const pendingTime = new Date().getTime() - callTime;
 
                 if (result.success) {             
-                    var replaceUrlParam = "{{ $replaceUrl ?? '' }}";
-                    var finalUrl = replaceUrlParam ? "/main?replaceUrl=" + replaceUrlParam : "/";       
+                    var replaceUrlParam = {!! json_encode($replaceUrl ?? '', JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT) !!};
+                    var finalUrl = getSafeRedirectUrl(replaceUrlParam);
                     if(pendingTime > 1100) {
                         document.querySelector('.splash').classList.remove('splash');
                         //location.replace("/main?replaceUrl={{ $replaceUrl ?? '' }}");
-                        location.replace(finalUrl);
+                        redirectAfterSignin(finalUrl);
                         //location.href = '/';
 //                        $('.splash').removeClass('splash');
                     } else {
                         setTimeout(() => {
                             document.querySelector('.splash').classList.remove('splash');
                             //location.replace("/main?replaceUrl={{ $replaceUrl ?? '' }}");
-                            location.replace(finalUrl);
+                            redirectAfterSignin(finalUrl);
                             //location.href = '/';
 //                            $('.splash').removeClass('splash');
                         }, (1100 - pendingTime));
@@ -288,6 +288,60 @@ function checkMobile(){
         @if(!isset($isweb) || $isweb != 'Y')
         location.replace('/?isweb=Y');
         @endif
+    }
+    function redirectAfterSignin(url) {
+        if (!url || url === '/' || url === window.location.origin + '/') {
+            location.replace('/');
+            return;
+        }
+
+        try {
+            sessionStorage.setItem('allfurnPushBackHome', 'Y');
+        } catch (e) {
+            console.log(e);
+        }
+
+        location.href = addPushBackHomeParam(url);
+    }
+
+    window.addEventListener('pageshow', function () {
+        try {
+            if (location.search.indexOf('replaceUrl=') !== -1 && sessionStorage.getItem('allfurnPushBackHome') === 'Y') {
+                sessionStorage.removeItem('allfurnPushBackHome');
+                location.replace('/');
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    function getSafeRedirectUrl(url) {
+        if (!url) {
+            return '/';
+        }
+
+        try {
+            const redirectUrl = new URL(url, window.location.origin);
+            const allowedHosts = ['all-furn.com', 'www.all-furn.com', window.location.hostname];
+
+            if (!allowedHosts.includes(redirectUrl.hostname)) {
+                return '/';
+            }
+
+            return redirectUrl.href;
+        } catch (e) {
+            return '/';
+        }
+    }
+
+    function addPushBackHomeParam(url) {
+        try {
+            const redirectUrl = new URL(url, window.location.origin);
+            redirectUrl.searchParams.set('pushBackHome', 'Y');
+            return redirectUrl.href;
+        } catch (e) {
+            return url;
+        }
     }
     </script>
 </body>
